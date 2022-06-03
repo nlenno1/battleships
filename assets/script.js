dfac37fbce2baeeba92abd40eb883fc5d386bfb1
Nick Lennon <nick.lennon@hotmail.co.uk>
2022-06-03 19:59:50 +0100

    feat: create board function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,30 @@
 BOARD = document.getElementById('board');
 
 function generate_board () {
+    amountOfRowCol = 5
     BOARD.style.backgroundColor = "gray";
+
+    squareWidth = BOARD.clientWidth / amountOfRowCol
+    BOARD.style.height = squareWidth + "px"
+    console.log(squareWidth)
+    console.log(squareWidth*amountOfRowCol)
+
+    for (let i = amountOfRowCol; i > 0; i--) {
+        const newRow = document.createElement("div")
+        newRow.setAttribute('class', 'row g-0')
+        newRow.style.height = squareWidth + "px"
+        newRow.style.width = "100%"
+        BOARD.appendChild(newRow)
+
+        for (let j = 1; j <= amountOfRowCol; j++) {
+            const newSquare = document.createElement("div")
+            newSquare.style.height = (squareWidth)  + "px"
+            newSquare.style.width = (squareWidth) + "px"
+            newSquare.setAttribute('class', 'board-square')
+            newSquare.setAttribute('id', j + "-" + i )
+            newRow.appendChild(newSquare);
+        }
+    }
 }
 
 window.onload = function() {
